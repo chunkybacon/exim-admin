4f31b34052e28c396923a3374f57bf0283450c23
Maxim Filatov <pipopolam@gmail.com>
2010-10-06 20:06:15 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EximAdmin::Application.config.secret_token = 'b90b3aff7c94b3c659627a8fd5adf2aac22e8e0fea81d5d41350cfd6ed9dd6d5bfe8317c3d9246ca439f1dfb79e911937c36b2428685e01870e5caa75416d1bd'
